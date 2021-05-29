04320d3765dbb23b669cec561b13a8cdb357a9dd
Alini Canedo <alini.canedo@gmail.com>
2021-05-29 20:15:15 -0300

    first commit

---
+++ resources/views/admin/home.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.admin')
+@section('title', 'Admin Home')
+
+@section('content')
+<p>Lorem ipsum dolor, sit amet consectetur adipisicing, elit. Sequi quae, fugit at, numquam ad libero atque, eaque explicabo tempore, expedita nostrum iure earum. Ipsam, repellendus, tenetur. Temporibus minus beatae et.</p>
+@endsection

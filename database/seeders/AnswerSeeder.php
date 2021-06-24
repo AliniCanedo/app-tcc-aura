c77be0b3841d6176058974bfd4edc5d54e672bd0
Alini Canedo <alini.canedo@gmail.com>
2021-06-24 12:24:08 -0300

    criando outras respostas de teste

--- database/seeders/AnswerSeeder.php
+++ database/seeders/AnswerSeeder.php
@@ -20,5 +20,53 @@ class AnswerSeeder extends Seeder
             'nao_sabe' => 1,
             'nao_aplica' => 1
         ]);
+        $newAnswer = Answer::create([
+            'id_matricula' => 198,
+            'id_classification' => 2,
+            'nao_sabe' => 2,
+            'nao_aplica' => 2
+        ]); 
+        $newAnswer = Answer::create([
+            'id_matricula' => 198,
+            'id_classification' => 3,
+            'nao_sabe' => 3,
+            'nao_aplica' => 3
+        ]);
+        $newAnswer = Answer::create([
+            'id_matricula' => 198,
+            'id_classification' => 4,
+            'nao_sabe' => 4,
+            'nao_aplica' => 4
+        ]);
+        $newAnswer = Answer::create([
+            'id_matricula' => 198,
+            'id_classification' => 5,
+            'nao_sabe' => 5,
+            'nao_aplica' => 5
+        ]);
+        $newAnswer = Answer::create([
+            'id_matricula' => 198,
+            'id_classification' => 6,
+            'nao_sabe' => 6,
+            'nao_aplica' => 6
+        ]);
+        $newAnswer = Answer::create([
+            'id_matricula' => 198,
+            'id_classification' => 7,
+            'nao_sabe' => 7,
+            'nao_aplica' => 7
+        ]);
+        $newAnswer = Answer::create([
+            'id_matricula' => 198,
+            'id_classification' => 8,
+            'nao_sabe' => 8,
+            'nao_aplica' => 8
+        ]);
+        $newAnswer = Answer::create([
+            'id_matricula' => 198,
+            'id_classification' => 9,
+            'nao_sabe' => 9,
+            'nao_aplica' => 9
+        ]);
     }
 }

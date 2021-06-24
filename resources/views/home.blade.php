b7c46dd30e8941364e2a54a61d1f445a855893db
Alini Canedo <alini.canedo@gmail.com>
2021-06-24 14:33:07 -0300

    <i class="fas fa-arrow-left"></i>

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,15 +3,15 @@
 @section('content')
     <div class="container">
         <div class="row">
-            <div class="col-md-12">
-                <h1>Bem Vindo</h1>
-                <p>Sistema de pesquisa de satisfação AURA</p>
+            <div class="col-md-12 mb-3">
+                <h1>Bem-Vindo!</h1>
+                <h4>Sistema de pesquisa de satisfação AURA</h4>
             </div>
         </div>
         <div class="row">
             <div class="col-md-12">
                 <p>Aqui você poderá avaliar nossa instituição de ensino.</p>
-                <p>Escolha um tema ao lado para responder</p>
+                <p><i class="fas fa-arrow-left"></i> Escolha um tema ao lado para responder.</p>
             </div>
         </div>
     </div>
